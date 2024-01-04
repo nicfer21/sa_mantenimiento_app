43b95492fb98f8c83b885f7568a0c9cb121483ed
nicfer21 <ferboncun@gmail.com>
2024-01-04 18:36:05 -0500

    End

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
-
-//para crear el servidor dentro del net
 /* export default defineConfig({
   plugins: [react()],
-  server: { host: true },
 }); */
+
+//para crear el servidor dentro del net
+export default defineConfig({
+  plugins: [react()],
+  server: { host: true },
+});
